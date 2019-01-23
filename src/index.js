6d67aa28d4992e02053c21dc9d5c3c767d2be6c8
Theis Frøhlich <theisof@gmail.com>
2019-01-23 16:12:49 +0100

    change babel transpilation

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@
 import PopupBox, { initPopupBox }  from './components/PopupBox'
 import Button from './components/Button'
 import Navigation from './components/Navigation'
+import Backdrop from './components/Backdrop'
 
 // Utils
 import domHelpers from './utils/domHelpers'
 import emitEvent from './utils/emitEvent'
 
 export {
+  Backdrop,
   PopupBox,
   initPopupBox,
   Button,
